ee362665fe488e7b7c0441e02bb6e0a7a06e8e47
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-04-17 13:01:11 +0800

    Switch test.sh from python to python3

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 json='{"nickname": "aaaa", "password": "bbbb"}'
-curl -X POST -H "Content-Type: application/json" -d "$json" http://localhost:3000/user/register | python -m json.tool
+curl -X POST -H "Content-Type: application/json" -d "$json" http://localhost:3000/user/register | python3 -m json.tool
